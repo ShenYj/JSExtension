2f8bf80493f292cbf80c191a82d45c198bdea719
ShenYj <shenyanjie123@foxmail.com>
2016-12-14 20:59:33 +0800

    新建工程

---
+++ JSExtension/JSExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSExtension
+//
+//  Created by ShenYj on 2016/12/14.
+//  Copyright © 2016年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSExtension/JSExtension/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSExtension
+//
+//  Created by ShenYj on 2016/12/14.
+//  Copyright © 2016年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
